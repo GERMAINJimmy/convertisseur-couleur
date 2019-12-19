847a6bd185d8486ab6da899b803376716852e452
Jimmy GERMAIN <germain.jimmy@gmail.com>
2019-12-19 17:22:23 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function fondRouge()
+{
+    var p = document.getElementById('para1');
+    p.style.backgroundColor="red";
+}
+
+function fondJaune()
+{
+    var p = document.getElementById('para1');
+    p.style.backgroundColor="yellow";
+}
